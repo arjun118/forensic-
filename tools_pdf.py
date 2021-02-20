332a6211a34f653e2dae817107cbdb9cf97759c6
arjun118 <vishnutejach3@gmail.com>
2021-02-20 15:15:11 +0530

    tools for pdf file,another tool is on its way

---
+++ tools_pdf.py
@@ -0,0 +1,30 @@
+import subprocess as sp
+
+
+def exif(file_location):
+	print('output from exiftool is:\n')
+	output=sp.run(['exiftool',file_location],capture_output=True,text=True)
+	print(output.stdout)
+
+
+def bw(file_location):
+	print('output from binwalk is:\n')
+	output=sp.run(['binwalk',file_location],capture_output=True,text=True)
+	print(output.stdout)
+
+
+
+def strs(file_location):
+	print('output from strings is:\n')
+	output=sp.run(['strings',file_location],capture_output=True,text=True)
+	print(output.stdout)
+
+
+
+def xxd(file_location):
+	print('output from xxd is:\n')
+	output=sp.run(['xxd','-p',file_location],capture_output=True,text=True)
+	print(output.stdout)
+
+
+
