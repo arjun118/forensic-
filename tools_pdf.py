6d3c95a23800f946e48a2dd5898ade6bf7fce428
arjun118 <78010223+arjun118@users.noreply.github.com>
2021-03-01 05:36:54 +0530

    Update tools_pdf.py

--- tools_pdf.py
+++ tools_pdf.py
@@ -4,27 +4,32 @@ import subprocess as sp
 def exif(file_location):
 	print('output from exiftool is:\n')
 	output=sp.run(['exiftool',file_location],capture_output=True,text=True)
-	print(output.stdout)
+	output=output.stdout
 
 
 def bw(file_location):
 	print('output from binwalk is:\n')
 	output=sp.run(['binwalk',file_location],capture_output=True,text=True)
-	print(output.stdout)
+	output=output.stdout
 
 
 
 def strs(file_location):
-	print('output from strings is:\n')
+	
 	output=sp.run(['strings',file_location],capture_output=True,text=True)
-	print(output.stdout)
+	output=output.stdout
 
 
 
 def xxd(file_location):
-	print('output from xxd is:\n')
+	
 	output=sp.run(['xxd','-p',file_location],capture_output=True,text=True)
-	print(output.stdout)
+	output=output.stdout
+	
+def peepdf(file_location):
+	
+	output=sp.run(['peepdf',file_location],capture_output=True,text=True)
+	output=output.stdout
 
 
 
