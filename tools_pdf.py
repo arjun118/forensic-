baff5cf5438d30d9e3ee3da73405ecb73d6ed1c4
arjun118 <78010223+arjun118@users.noreply.github.com>
2021-03-01 05:40:13 +0530

    Update tools_pdf.py

--- tools_pdf.py
+++ tools_pdf.py
@@ -2,15 +2,32 @@ import subprocess as sp
 
 
 def exif(file_location):
-	print('output from exiftool is:\n')
+	
 	output=sp.run(['exiftool',file_location],capture_output=True,text=True)
 	output=output.stdout
+	with open('output.txt','a') as f:
+		f.write('\n\noutput from exiftool :\n\n')
+		for i in output:
+			try:
+				
+				f.write(i)
+			except TypeError:
+				print('Extracted')
+	
 
 
 def bw(file_location):
-	print('output from binwalk is:\n')
+	
 	output=sp.run(['binwalk',file_location],capture_output=True,text=True)
 	output=output.stdout
+	with open('output.txt','a') as f:
+		f.write('\n\noutput from binwalk :\n\n')
+		for i in output:
+			try:
+				
+				f.write(i)
+			except TypeError:
+				print('Extracted')
 
 
 
@@ -18,6 +35,14 @@ def strs(file_location):
 	
 	output=sp.run(['strings',file_location],capture_output=True,text=True)
 	output=output.stdout
+	with open('output.txt','a') as f:
+		f.write('\n\noutput from strings :\n\n')
+		for i in output:
+			try:
+				
+				f.write(i)
+			except TypeError:
+				print('Extracted')
 
 
 
@@ -25,11 +50,27 @@ def xxd(file_location):
 	
 	output=sp.run(['xxd','-p',file_location],capture_output=True,text=True)
 	output=output.stdout
+	with open('output.txt','a') as f:
+		f.write('\n\noutput from xxd :\n\n')
+		for i in output:
+			try:
+				
+				f.write(i)
+			except TypeError:
+				print('Extracted')
 	
 def peepdf(file_location):
 	
 	output=sp.run(['peepdf',file_location],capture_output=True,text=True)
 	output=output.stdout
+	with open('output.txt','a') as f:
+		f.write('\n\noutput from peepdf :\n\n')
+		for i in output:
+			try:
+				
+				f.write(i)
+			except TypeError:
+				print('Extracted')
 
 
 
